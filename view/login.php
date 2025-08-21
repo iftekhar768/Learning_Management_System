2f39c234c3a6a3e34c31194aa7345bae4a1fa300
iftekhar768 <iftekharobito10@gmail.com>
2025-08-21 15:55:25 +0600

    Project Started

---
+++ view/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <h1>Log In</h1>
+    <form action="">
+        <label for="UserName">UserName: </label>
+        <input type="text" id="UserName"><br><br>
+        <label for="Password">Password: </label>
+        <input type="password" id="Password"><br><br>
+        <input type="submit" value="submit">
+        <button> <a href="registration.php">Registration</a></button>
+        <br><br>
+
+        <a href="">Can't Access your Account?</a>
+    </form>
+    
+</body>
+</html>
---
+++ view/registration.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Registration</h1>
+    <form action="">
+        <label for="UserName">User Name :</label>
+        <input type="text" id="UserName"><br><br>
+        <label for="Email">Email : </label>
+        <input type="email" id="Email"><br><br>
+        <label for="Phone">Phone : </label>
+        <input type="number" id="Phone"> <br><br>
+        <label for="bod">Date of Birth :</label>
+        <input type="date" id="bod"> <br><br>
+        <label for="Password">Password :</label>
+        <input type="password" id="Password"> <br><br>
+        <label for="CPassword">Confirm Password :</label>
+        <input type="password" id="CPassword"> <br><br>
+        <input type="submit" value="submit">
+        <button> <a href="login.php">Login</a></button>
+
+    </form>
+</body>
+</html>
