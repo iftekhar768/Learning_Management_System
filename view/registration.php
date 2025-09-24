b4c1d9d2f07e8fe0cede61a7f6b9ad063d86942e
iftekhar768 <iftekharobito10@gmail.com>
2025-09-25 04:36:08 +0600

    some css changes to registration page

--- view/registration.php
+++ view/registration.php
@@ -9,23 +9,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
+    <link rel="stylesheet" href="../assets/css/regstyle.css">
 </head>
 <body>
+    <div class="container">
     <form action="../controller/authController.php" method="POST">
-        <label for="userId">User ID:</label>
-        <input type="text" name="userId" id="userId"><br><br>
-        <label for="username">Name: </label>
-        <input type="text" name="username" id="username"><br><br>
-        <label for="roleSelect">Role: </label>
-        <input type="radio" name="roleSelect" value="teacher" id="roleSelect">Teacher
-        <input type="radio" name="roleSelect" value="student" id="roleSelect">Student
-        <br><br>
-        <label for="email">Email: </label>
-        <input type="text" name="email" id="email"><br><br>
-        <label for="pass">Password: </label>
-        <input type="text" name="pass" id="pass"><br><br>
-        <input type="submit" name="submit" value="register"><br>
-         
+        <div class="listContainer">
+            <label for="userId">User ID:</label>
+            <input type="text" name="userId" id="userId"><br><br>
+            <label for="username">Name: </label>
+            <input type="text" name="username" id="username"><br><br>
+            <label for="roleSelect">Role: </label>
+            <input type="radio" name="roleSelect" value="teacher" id="roleSelect">Teacher
+            <input type="radio" name="roleSelect" value="student" id="roleSelect">Student
+            <br><br>
+            <label for="email">Email: </label>
+            <input type="text" name="email" id="email"><br><br>
+            <label for="pass">Password: </label>
+            <input type="text" name="pass" id="pass"><br><br>
+            <input type="submit" name="submit" value="register" id="submitBtn"><br>
+        </div>     
     </form>
+    </div>
 </body>
 </html>
