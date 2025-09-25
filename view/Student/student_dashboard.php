8d1eba63568262d596219fa6c6f3ec0f40870a94
Md. Rehab Ansary Yasin <22-48434-3@student.aiub.edu>
2025-09-25 13:51:42 +0600

    Add files via upload

---
+++ view/Student/student_dashboard.php
@@ -0,0 +1,40 @@
+<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Student Dashboard</title>
+  <link rel="stylesheet" href="Studentstyle.css">
+</head>
+<body>
+  <div class="sidebar">
+    <h2>Student</h2>
+    <ul>
+      <li><a href="student_dashboard.php">Dashboard</a></li>
+      <li><a href="student_courses.php">Courses</a></li>
+      <li><a href="student_announcements.php">Announcements</a></li>
+    </ul>
+  </div>
+  <div class="main">
+    <h1>Student Dashboard</h1>
+    <div class="cards">
+      <div class="card">
+        <img src="https://img.icons8.com/ios-filled/50/000000/open-book.png"/>
+        <h3>Enroll in Course</h3>
+        <p>Enroll in available courses and track progress</p>
+      </div>
+      <div class="card">
+        <img src="https://img.icons8.com/ios-filled/50/000000/test.png"/>
+        <h3>View Quizzes</h3>
+        <p>Attempt quizzes and view scores instantly</p>
+      </div>
+      <div class="card">
+        <img src="https://img.icons8.com/ios-filled/50/000000/document.png"/>
+        <h3>Submit Assignments</h3>
+        <p>Submit/download course materials</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+?>
