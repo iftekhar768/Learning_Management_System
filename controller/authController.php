0ad3aac67306ea49412bd218f67d526dc597d328
iftekhar768 <iftekharobito10@gmail.com>
2025-10-06 23:15:31 +0600

    Course structure making

--- controller/authController.php
+++ controller/authController.php
@@ -42,9 +42,6 @@
         }
     }
 
-  
-
- 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["insert"]))
     {
         $user = [
---
+++ view/Admin/courses.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Course Management</h1>
+    <form action="" method="POST">
+        <input type="text" name="course_name" placeholder="course_name" required>
+        <input type="text" name="course_description" placeholder="course_description">
+        
+    </form>
+</body>
+</html>
